db703dda45188a24da9505a7443d48579ebb0193
Jeet Prakash <Jeet1989a@gmail.com>
2021-02-28 17:06:42 +0530

    Initializing the project

---
+++ src/test/java/com/jeetprksh/reddimg/ReddimgTest.java
@@ -0,0 +1,20 @@
+package com.jeetprksh.reddimg;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ReddimgTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
